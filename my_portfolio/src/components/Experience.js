f4bee37cc2c86b05ffcec6e4b7deba200253ded9
sachin singh <sachinsingh000094@gmail.com>
2023-01-22 17:58:52 +0530

    Changes

--- my_portfolio/src/components/Experience.js
+++ my_portfolio/src/components/Experience.js
@@ -1,63 +1,29 @@
 const Experience = () => {
     return (
         <div className="experience">
-            <h2>Experience</h2>
+            <h2>Education</h2>
 
             <ul>
                 <li>
-                    <i>Unifarm, Oropocket</i> - <strong>Blockchain Developer</strong>,
-                    July 2022-Present
+                    <i> Global Institute Of Technology And Management</i>
                     <ul>
                         <li>
-                            Upgraded and Deployed Smart Contracts on Polygon, Binance, Fantom
-                            and Ethereum testnet & Mainnet.
+                            <strong>Bachelor of Technology in Computer Science, </strong>
                         </li>
                         <li>
-                            Involved in the global blockchain community—worked on implementing
-                            and integrating the latest improvement proposals.
+                            July 2020-Present
                         </li>
-                        <li>
-                            Collaborated with Design, Product and Frontend teams to ensure
-                            proposed Backend data structure met requirements.
-                        </li>
-                        <li>
-                            Built cross-chain bridges and architecture for various Blockchain
-                            solutions.
-                        </li>
-                        <li>
-                            Gasless Implementation of functions in Contracts and Frontend Code
-                            with Biconomy.
-                        </li>
-                        <li>
-                            {" "}
-                            Involved in writing Smart Contracts and Backend Integration of all
-                            the modules for the DAPPS.
-                        </li>
-                        <li> Developed a Telegram Bot for the UniFarm telegram channel.</li>
-                        <li>
-                            {" "}
-                            Integrated various Wallets such as Coinbase, Hashpack wallet in
-                            the project.
-                        </li>
-                        <li>
-                            Setup the Graph servers for projects and indexing the data
-                            whenever required.
-                        </li>
-                        <li>
-                            {" "}
-                            Carried out the competetive research on various topics & platforms
-                            for the DAPPS project.
-                        </li>
+
+
                     </ul>
                 </li>
                 <li>
-                    <i>Code Eater</i> - <strong>Blockchain Intern</strong>, April 2022 -
-                    June 2022
+                    <i>M.M Public School, Gurugram - Secondary Education
+                    </i>
                     <ul>
-                        <li>Studied Smart Contracts development.</li>
-                        <li>Deploying contracts on local Blockchain.</li>
-                        <li>Learnt about Truffle, Ganache, Web3.js and other tools.</li>
-                        <li>Studied the concept of Defi &amp; other Web3 Products.</li>
+                        <li> <strong>Science With Maths</strong></li>
+                        <li>April 2019 -June 2020</li>
+
                     </ul>
                 </li>
             </ul>
--- my_portfolio/src/components/Projects.js
+++ my_portfolio/src/components/Projects.js
@@ -1,7 +1,7 @@
 // Import Assets
 import uniswap from '../assets/uniswap.png';
 import compound from '../assets/compound.png';
-import aave from '../assets/aave.png';
+
 
 const Projects = () => {
     return (
@@ -11,30 +11,26 @@ const Projects = () => {
             <div className="projects__cards">
 
                 <div className="projects__card">
-                    <h3>DAPPS</h3>
+                    <h3>Jarvis- Voice Assistant</h3>
                     <img src={uniswap} alt="Uniswap Swap Page" />
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                        Facilis consectetur facere neque assumenda quasi aliquid,
-                        perferendis ratione rerum dolorem.
+                    <p>Voice Assistant that can convert audio to speeches, perform various operations on instructions given.
                     </p>
 
-                    <a href="http://165.22.213.170/" target="_blank" className="button">Site</a>
-                    <a href="https://github.com/vipinsinghh" target="_blank" className="button">Code</a>
+                    {/* <a href="http://165.22.213.170/" target="_blank" className="button">Site</a>
+                    <a href="https://github.com/vipinsinghh" target="_blank" className="button">Code</a> */}
                 </div>
 
                 <div className="projects__card">
-                    <h3>UniFarm</h3>
+                    <h3>FlipKart Clone - Website</h3>
                     <img src={compound} alt="Compound Landing Page" />
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                        Facilis consectetur facere neque assumenda quasi aliquid,
-                        perferendis ratione rerum dolorem.
+                    <p>Created a FlipKart clone main web page using frontend development concepts.
                     </p>
 
-                    <a href="https://app.unifarm.co/" target="_blank" className="button">Site</a>
-                    <a href="https://github.com/vipinsinghh" target="_blank" className="button">Code</a>
+                    {/* <a href="https://app.unifarm.co/" target="_blank" className="button">Site</a>
+                    <a href="https://github.com/vipinsinghh" target="_blank" className="button">Code</a> */}
                 </div>
 
-                <div className="projects__card">
+                {/* <div className="projects__card">
                     <h3>NFT Marketplace</h3>
                     <img src={aave} alt="Aave Landing Page" />
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
@@ -44,7 +40,7 @@ const Projects = () => {
 
                     <a href="https://ethereum.org/en/nft/" target="_blank" className="button">Site</a>
                     <a href="https://github.com/vipinnsingh/Solidity/blob/main/digitalmarketplace.sol" target="_blank" className="button">Code</a>
-                </div>
+                </div> */}
             </div>
         </section>
     );
